cb134df939f9682027970aa3b8c40279427804f1
Unknown6656 <unknown84@gmx.de>
2018-03-05 23:19:28 +0100

    initial code commit

---
+++ OpenTKMinecraft/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System;
+
+using OpenTK.Graphics.OpenGL4;
+using OpenTK;
+using OpenTKTestRenderer.Components;
+
+namespace OpenTKTestRenderer
+{
+    public static unsafe class Program
+    {
+        public const int GL_VERSION_MAJ = 4;
+        public const int GL_VERSION_MIN = 6;
+
+
+        [STAThread]
+        public static int Main(string[] args)
+        {
+            int ret = 0;
+
+            try
+            {
+                InnerMain(args, ref ret);
+            }
+            catch (Exception ex)
+            {
+                StringBuilder sb = new StringBuilder();
+
+                ret = ex.HResult;
+
+                if (ret == 0)
+                    ret = -1;
+
+                while (ex != null)
+                {
+                    sb.Insert(0, $"{ex.Message}:\n{ex.StackTrace}\n");
+
+                    ex = ex.InnerException;
+                }
+
+                Console.WriteLine(sb.ToString());
+            }
+
+            if (Debugger.IsAttached && (ret != 0))
+            {
+                Console.WriteLine("Press any key to exit ...");
+                Console.ReadKey(true);
+            }
+
+            return ret;
+        }
+
+        public static void InnerMain(string[] args, ref int exit)
+        {
+            using (MainWindow win = new MainWindow(args)
+            {
+                Width = 1280,
+                Height = 720,
+                Title = $"Test renderer [OpenGL v.{GL.GetString(StringName.Version)}]"
+            })
+                win.Run(120);
+        }
+    }
+}
---
+++ OpenTKMinecraft/Shaders/gshader.geom
@@ -0,0 +1,9 @@
+﻿#version 440 core
+
+out vec4 color;
+
+
+void main()
+{
+    color = vec4(1.0, 0.0, 0.0, 1.0);
+}
